9e833f957066ec91f3df49f4f2d2f42b272a0e67
Cotulorai <cotulorai@outlook.com>
2020-04-02 23:32:01 +0800

    modify some files

--- app/src/main/java/com/codinlog/album/adapter/kotlin/AlbumSlidePlayRVFullAdapter.kt
+++ app/src/main/java/com/codinlog/album/adapter/kotlin/AlbumSlidePlayRVFullAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bm.library.PhotoView
 import com.bumptech.glide.Glide
 import com.codinlog.album.R
 import com.codinlog.album.application.AlbumApplication
