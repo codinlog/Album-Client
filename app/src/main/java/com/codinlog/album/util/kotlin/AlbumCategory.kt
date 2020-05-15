4f15e66fd3f0e54a0197c98804c0f71df6339426
codinlog <codinlog@foxmail.com>
2020-05-15 17:09:25 +0800

    a

--- app/src/main/java/com/codinlog/album/util/kotlin/AlbumCategory.kt
+++ app/src/main/java/com/codinlog/album/util/kotlin/AlbumCategory.kt
@@ -35,18 +35,22 @@ class AlbumCategory(private val pair: Pair<List<PhotoBean>, List<CategoryEntity>
             val removeList = pair.second.filter { c -> pair.first.all { p -> c.photoId != p.photoId } }.toList()
             val classifiedList = mutableListOf<CategoryEntity>()
             todoList.forEach {
-                val bitmap = BitmapFactory.decodeFile(it.photoPath).copy(Bitmap.Config.ARGB_8888, true)
-                if (bitmap != null) {
-                    val recognitions = classifier!!.recognizeImage(bitmap, 0)
-                    if (recognitions[0] != null) {
-                        val categoryEntity = CategoryEntity()
-                        categoryEntity.photoId = it.photoId
-                        if (recognitions[0].confidence >= 0.7)
-                            categoryEntity.category = recognitions[0].title
-                        else
-                            categoryEntity.category = ""
-                        classifiedList.add(categoryEntity)
+                try {
+                    val bitmap = BitmapFactory.decodeFile(it.photoPath).copy(Bitmap.Config.ARGB_8888, true)
+                    if (bitmap != null) {
+                        val recognitions = classifier!!.recognizeImage(bitmap, 0)
+                        if (recognitions[0] != null) {
+                            val categoryEntity = CategoryEntity()
+                            categoryEntity.photoId = it.photoId
+                            if (recognitions[0].confidence >= 0.7)
+                                categoryEntity.category = recognitions[0].title
+                            else
+                                categoryEntity.category = ""
+                            classifiedList.add(categoryEntity)
+                        }
                     }
+                } catch (e: Exception) {
+
                 }
             }
             withContext(Dispatchers.IO) {
